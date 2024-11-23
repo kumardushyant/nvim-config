0c4afff3e2281a6d0b181be728f8f9d81a39daef
Dushyant Kumar <dushyant.kumar@gmail.com>
2024-11-23 17:12:14 +0000

    Removed WSLClipboard

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -42,17 +42,3 @@ vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comme
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
 
 vim.g.have_nerd_font = true
-
-vim.opt.clipboard = "unnamedplus"
-vim.g.clipboard = {
-    name = "WSLClipboard",
-    copy = {
-         ["+"] = "clip.exe",
-         ["*"] = "clip.exe"
-    },
-    paste = {
-        ["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
-        ["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))"
-    },
-    cache_enabled = 0 
-}
